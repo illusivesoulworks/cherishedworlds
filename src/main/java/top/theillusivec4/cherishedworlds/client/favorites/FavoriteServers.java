c9c0eabef71014885592430659c4c55522112e33
TheIllusiveC4 <29991504+TheIllusiveC4@users.noreply.github.com>
2021-07-01 02:13:38 -0700

    finish server integration, closes #8

--- src/main/java/top/theillusivec4/cherishedworlds/client/favorites/FavoriteServers.java
+++ src/main/java/top/theillusivec4/cherishedworlds/client/favorites/FavoriteServers.java
@@ -1,13 +1,9 @@
 package top.theillusivec4.cherishedworlds.client.favorites;
 
-import java.util.ArrayList;
-import java.util.List;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.MultiplayerScreen;
 import net.minecraft.client.gui.screen.ServerSelectionList;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.client.multiplayer.ServerData;
-import net.minecraft.client.multiplayer.ServerList;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import top.theillusivec4.cherishedworlds.mixin.core.MultiplayerScreenAccessor;
 
@@ -73,7 +69,12 @@ public class FavoriteServers implements IFavoritesManager<MultiplayerScreen> {
             }
             FavoritesList.save();
             accessor.getSelectionList().updateOnlineServers(screen.getServerList());
-            evt.setCanceled(true);
+            ServerSelectionList.Entry selected = accessor.getSelectionList().getSelected();
+
+            if (selected instanceof ServerSelectionList.NormalEntry) {
+              disableDeletion((ServerSelectionList.NormalEntry) selected,
+                  accessor.getDeleteButton());
+            }
             return;
           }
         }
@@ -83,70 +84,12 @@ public class FavoriteServers implements IFavoritesManager<MultiplayerScreen> {
 
   @Override
   public void clicked(MultiplayerScreen screen) {
-//    MultiplayerScreenAccessor accessor = (MultiplayerScreenAccessor) screen;
-//    ServerSelectionList selectionList = accessor.getSelectionList();
-//
-//    if (selectionList != null) {
-//      ServerSelectionList.Entry entry = selectionList.getSelected();
-//
-//      if (entry instanceof ServerSelectionList.NormalEntry) {
-//        Button deleteButton = accessor.getDeleteButton();
-//
-//        if (deleteButton != null) {
-//          disableDeletion((ServerSelectionList.NormalEntry) entry, deleteButton);
-//        }
-//      }
-//    }
+    // NO-OP
   }
 
   @Override
   public int getOffset() {
     return 168;
-  }
-
-  private static void refreshList(MultiplayerScreen multiplayerScreen,
-                                  ServerSelectionList serverSelectionList) {
-//    Minecraft mc = Minecraft.getInstance();
-//    ServerList serverList = new ServerList(mc);
-//    List<ServerSelectionList.Entry> entries = serverSelectionList.getEventListeners();
-//    entries.clear();
-//    List<ServerData> serverData = new ArrayList<>();
-//
-//    for (int i = 0; i < serverList.countServers(); i++) {
-//      serverData.add(serverList.getServerData(i));
-//    }
-//    List<ServerData> favorites = new ArrayList<>();
-//    List<ServerData> list = new ArrayList<>();
-//
-//    for (ServerData server : serverData) {
-//
-//      if (FavoritesList.contains(server.serverName + server.serverIP)) {
-//        favorites.add(server);
-//      } else {
-//        list.add(server);
-//      }
-//    }
-//    int index = 0;
-//
-//    for (ServerData server : favorites) {
-//      serverList.set(index, server);
-//      index++;
-//    }
-//
-//    for (ServerData server : list) {
-//      serverList.set(index, server);
-//      index++;
-//    }
-//    serverSelectionList.updateOnlineServers(serverList);
-//    ServerSelectionList.Entry entry = serverSelectionList.getSelected();
-//
-//    if (entry instanceof ServerSelectionList.NormalEntry) {
-//      Button deleteButton = ((MultiplayerScreenAccessor) multiplayerScreen).getDeleteButton();
-//
-//      if (deleteButton != null) {
-//        disableDeletion((ServerSelectionList.NormalEntry) entry, deleteButton);
-//      }
-//    }
   }
 
   private static void disableDeletion(ServerSelectionList.NormalEntry entry, Button deleteButton) {
