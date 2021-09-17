1397167b732965adeebd8e9cab2c0e9a46ac420b
TheIllusiveC4 <29991504+TheIllusiveC4@users.noreply.github.com>
2021-09-16 22:46:09 -0700

    update build and copyright

--- forge/src/main/java/top/theillusivec4/cherishedworlds/mixin/core/MultiplayerScreenAccessor.java
+++ forge/src/main/java/top/theillusivec4/cherishedworlds/mixin/core/MultiplayerScreenAccessor.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2018-2021  C4
+ *
+ * This file is part of Cherished Worlds, a mod made for Minecraft.
+ *
+ * Cherished Worlds is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or any later version.
+ *
+ * Cherished Worlds is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with Cherished
+ * Worlds.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package top.theillusivec4.cherishedworlds.mixin.core;
 
 import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
